bd32bf40673d3772dd4175301a3ece6fde699d91
Sharp <sharp.zhou@shopee.com>
2021-08-05 15:43:19 +0800

    t

---
+++ m.js
@@ -0,0 +1,48 @@
+// const PRIORITY_MAP = {
+//   1: {
+//     text: "\ue659",
+//     fill: "#FF1403",
+//   },
+//   2: {
+//     text: "\ue658",
+//     fill: "#0274FF",
+//   },
+//   3: {
+//     text: "\ue657",
+//     fill: "#06AF00",
+//   },
+// };
+
+
+
+    // if (cfg.priority && PRIORITY_MAP[cfg.priority]) {
+    //   group.addShape("text", {
+    //     attrs: {
+    //       x: formattedPadding[3],
+    //       y: -formattedPadding[1],
+    //       fontFamily: "iconfont",
+    //       textAlign: "center",
+    //       textBaseline: "middle",
+    //       fontSize: fontSize * 1.5,
+    //       ...PRIORITY_MAP[cfg.priority],
+    //     },
+    //   });
+    // }
+
+    // if (cfg.remark) {
+    //   group.addShape("text", {
+    //     name: NodeName.Remark,
+    //     attrs: {
+    //       x: fontSize * 1.5 + formattedPadding[3],
+    //       y: -formattedPadding[1],
+    //       fontFamily: "iconfont",
+    //       textAlign: "center",
+    //       textBaseline: "middle",
+    //       fontSize,
+    //       text: "\ue63d",
+    //       fill: "#333",
+    //       cursor: "pointer",
+    //     },
+    //   });
+    // }
+
